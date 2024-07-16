eb733e4a063294c8f3f41b0e8b9881a509c981c9
AdityaKumarAditya <aaditya.khokar@gmail.com>
2024-07-16 23:56:42 +0200

    Initial commit

---
+++ src/main/java/gmail/selenium/test/selenium/test/App.java
@@ -0,0 +1,13 @@
+package gmail.selenium.test.selenium.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
